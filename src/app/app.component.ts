5e14c43f80041a0c5e4dfb7b60af85794557f2af
fahad2021 <fahadrahman810@gmail.com>
2022-04-12 13:09:17 +0600

    create movies cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,44 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.moviesThreaders=[{
-      title: "spiderman1",
+      title: "Spider Man1",
       releasedate: new Date(),
-      price: 50.00
+      price: 50.00,
+      poster:"https://m.media-amazon.com/images/M/MV5BMTY5NzM3MTYyNV5BMl5BanBnXkFtZTgwNDE0ODkxMjI@._V1_UY100_CR23,0,100,100_AL_.jpg"
     },
     {
-      title: "spiderman1",
+      title: "Monna",
       releasedate: new Date(),
-      price: 50.00
-    },
+      price: 50.00,
+      poster:"https://m.media-amazon.com/images/M/MV5BNTEzNzU3Mjg3NF5BMl5BanBnXkFtZTgwNzEyODY2MDI@._V1_UY100_CR25,0,100,100_AL_.jpg"
+    },    
     {
-      title: "spiderman1",
+      title: "Venhalsing",
       releasedate: new Date(),
-      price: 50.00
-    },
-    {
-      title: "spiderman1",
-      releasedate: new Date(),
-      price: 50.00
-    },
-    {
-      title: "spiderman1",
-      releasedate: new Date(),
-      price: 50.00
-    },
-    {
-      title: "spiderman2",
-      releasedate: new Date(),
-      price: 250.00
-    },
-    {
-      title: "spiderman3",
-      releasedate: new Date(),
-      price: 400.00
-    },
-    {
-      title: "spiderman4",
-      releasedate: new Date(),
-      price: 550.00
+      price: 550.00,
+      poster:"https://m.media-amazon.com/images/M/MV5BMTMzMTY3NjMxNV5BMl5BanBnXkFtZTcwNjk5NDQyMw@@._V1_UX100_CR0,0,100,100_AL_.jpg"
     }];
     this.moviesFuturerelease=[]
     // this.moviesFuturerelease=[{
